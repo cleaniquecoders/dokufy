c74bddb394937388ae5e593e5cfced4a84c939b6
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2026-01-15 15:15:10 +0800

    Refactor package structure and update metadata for Cleanique Coders/Dokufy

--- src/Commands/DokufyCommand.php
+++ src/Commands/DokufyCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton\Commands;
+namespace CleaniqueCoders\Dokufy\Commands;
 
 use Illuminate\Console\Command;
 
-class SkeletonCommand extends Command
+class DokufyCommand extends Command
 {
-    public $signature = 'skeleton';
+    public $signature = 'dokufy';
 
     public $description = 'My command';
 
---
+++ src/Dokufy.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace CleaniqueCoders\Dokufy;
+
+class Dokufy {}
